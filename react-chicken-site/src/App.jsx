115ee5b48c80ead8d8c8fd3f6e7ffda183c38427
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-06-02 17:39:17 +0200

    create chicken card component

--- react-chicken-site/src/App.jsx
+++ react-chicken-site/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import ChickenCard from "./components/ChickenCard"
 
 function App() {
   
   return (
     <>
-      
+
+    <div>
+      <h1>// Kingdom Of Chicken //</h1>
+    </div>
+
+    <div className='chickencard-container'>
+      <ChickenCard />
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-chicken-site/src/components/ChickenCard.jsx
@@ -0,0 +1,71 @@
+import './ChickenCard.css'
+
+const chickenList = [
+    {
+      name: "Le parrain",
+      imgSrc:
+      "https://images.pexels.com/photos/458825/pexels-photo-458825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      citations: "Je suis loin d'être mafieux, mais j'agirai comme un parrain si tu touches à mes poulettes!!!."
+    },
+    {
+        name: "la maitresse",
+        imgSrc:
+        "https://images.pexels.com/photos/15879767/pexels-photo-15879767/free-photo-of-mode-main-eau-ete.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        citations: "Ouuuups je voulais trouver une poule de luxe mais il y'a plusieurs interpretations!!!"
+      },
+      {
+        name: "La fille du parrain",
+        imgSrc:
+        "https://images.pexels.com/photos/5302686/pexels-photo-5302686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        citations: "oui oui oui oui c'est pas une poule mais elle a été adoptée!!!"
+      },
+      {
+        name: "la marraine",
+        imgSrc:
+        "https://cdn.pixabay.com/photo/2018/10/12/00/08/chicken-3741195_1280.jpg",
+        citations: "vous avez peur de mon mari??? Alors imaginez que c'est moi qui décide à la maison!!!"
+      },
+      {
+        name: "commissaire poulet",
+        imgSrc:
+        "https://images.unsplash.com/photo-1471623817296-aa07ae5c9f47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        citations: "huuuuuuum il faut absolument que l'on retrouve le coupable de cet horrible meurtre..."
+      },
+      {
+        name: "la victime du parrain",
+        imgSrc:
+        "https://cdn.pixabay.com/photo/2010/12/10/08/chicken-nuggets-1108_1280.jpg",
+        citations: "C'est insoutenable ce meurtre est hooooooorrrrriiiiible!!!!"
+      },
+      {
+        name: "le poulet de main ",
+        imgSrc:
+        "https://img.freepik.com/photos-premium/coq-va-au-poulailler-dans-village_231786-547.jpg?w=826",
+        citations: "Si il y'a un problème a régler c'est moi que l'on envoi!!!"
+      },
+      {
+        name: "l'inconnue",
+        imgSrc:
+        "https://cdn.pixabay.com/photo/2015/09/25/17/02/chicken-957795_1280.jpg",
+        citations: "Il fallait une huitième image du coup je ne dévoilerai pas mon identité HA! HA! HA!"
+      },
+  ];
+
+function ChickenCard() {
+     let chicken = chickenList[0];
+
+    return(
+        <>
+      <div className='chicken-card'>  
+        <figure>{chicken.imgSrc != null ? <img src={chicken.imgSrc} alt ={chicken.name}/> : <p>???</p>}
+        <figcaption>{chicken.name}</figcaption>
+        </figure>
+        <p>
+            {chicken.citations}
+        </p>
+      </div>
+      </>  
+    )
+  }
+  
+  export default ChickenCard;
